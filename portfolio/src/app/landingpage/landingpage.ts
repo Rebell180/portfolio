068001b239a33ada9e80dc4e3dbab9eebe69409c
Rebell180 <buchmann_marcel@web.de>
2025-09-09 20:33:17 +0200

    add structure, landingpage an background of hero and some basic styles

---
+++ portfolio/src/app/landingpage/landingpage.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Hero } from "./hero/hero";
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [Hero],
+  templateUrl: './landingpage.html',
+  styleUrl: './landingpage.scss'
+})
+export class Landingpage {
+
+}
