afe3526ca4255175611e88560f722ff8ecbb2b91
Rebell180 <buchmann_marcel@web.de>
2025-09-10 22:20:25 +0200

    animate button getintouch and hover of profession added and polaroid added as Background img

--- portfolio/src/app/landingpage/hero/hero.ts
+++ portfolio/src/app/landingpage/hero/hero.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.html',
-  styleUrl: './hero.scss'
+  styleUrls: ['./hero.scss']
 })
-export class Hero {
+export class Hero implements OnInit{
 
+  profession_special: string= "Frontend";
+  profession: string = "DEVELOPER";
+
+  ngOnInit(): void {
+
+    
+  }
+
+  setOnClickStyle () {
+    let btnRef = document.getElementById("btn-getintouch");
+    if (btnRef != null) {
+      btnRef.style.boxShadow.includes("none");// '@include mixins.boxSh();';
+    }
+  }
+
+  setOnClickReleaseStyle () {
+    let btnRef = document.getElementById("btn-getintouch");
+    if (btnRef != null) {
+      btnRef.style.boxShadow.includes("mixins.boxSh();");// '@include mixins.boxSh();';
+    }
+  }
 }
