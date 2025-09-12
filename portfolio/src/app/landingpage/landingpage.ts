cad77462eb89b1b5004704a540dcd8ecee689735
Rebell180 <buchmann_marcel@web.de>
2025-09-12 19:59:38 +0200

    first steps for header and navbar, add images, styles animations missing

--- portfolio/src/app/landingpage/landingpage.ts
+++ portfolio/src/app/landingpage/landingpage.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from "./hero/hero";
+import { Header } from "../shared/singlecomponents/header/header";
 
 @Component({
   selector: 'app-landingpage',
-  imports: [Hero],
+  imports: [Hero, Header],
   templateUrl: './landingpage.html',
   styleUrl: './landingpage.scss'
 })
